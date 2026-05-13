1975e9109b1109b6f91db5c705a1681d1907a1a7
Olexandr Zaporozhets <oleksandr.zaporozhets@nure.ua>
2026-05-13 14:38:19 +0300

    add pract2, fix pract1

--- Pract1/apz-pzpi-23-7-zaporozhets-oleksandr-pract1/code-examples-pract1.cs
+++ Pract1/apz-pzpi-23-7-zaporozhets-oleksandr-pract1/code-examples-pract1.cs
@@ -1,71 +1,71 @@
 //Приклад прямого створення об’єкта:
-1  public class User
-2  {
-3      public string Name { get; }
-4      public string Email { get; }
-5      public int Age { get; }
-6  
-7      public User(string name, string email, int age)
-8      {
-9          Name = name;
-10          Email = email;
-11          Age = age;
-12      }
-13  }
-14  
-15  User user = new User("Sasha", "email", 18);
+  public class User
+  {
+      public string Name { get; }
+      public string Email { get; }
+      public int Age { get; }
+ 
+      public User(string name, string email, int age)
+     {
+          Name = name;
+          Email = email;
+          Age = age;
+      }
+  }
+  
+  User user = new User("Sasha", "email", 18);
 
 //Product та інтерфейс Builder:
-1  public class Computer
-2  {
-3      public string CPU { get; set; }
-4      public string RAM { get; set; }
-5      public string Storage { get; set; }
-6      public bool HasGraphicsCard { get; set; }
-7  }
-8  
-9  public interface IComputerBuilder
-10  {
-11      void SetCPU();
-12      void SetRAM();
-13      void SetStorage();
-14      void SetGraphicsCard();
-15      Computer GetResult();
-16  }
+  public class Computer
+  {
+      public string CPU { get; set; }
+     public string RAM { get; set; }
+      public string Storage { get; set; }
+      public bool HasGraphicsCard { get; set; }
+  }
+  
+  public interface IComputerBuilder
+  {
+      void SetCPU();
+      void SetRAM();
+      void SetStorage();
+     void SetGraphicsCard();
+      Computer GetResult();
+  }
 
 //oncreteBuilder та Director:
-1  public class GamingComputerBuilder : IComputerBuilder
-2  {
-3      private Computer _computer = new Computer();
-4  
-5      public void SetCPU() => _computer.CPU = "Intel Core i7";
-6      public void SetRAM() => _computer.RAM = "32 GB";
-7      public void SetStorage() => _computer.Storage = "1 TB SSD";
-8      public void SetGraphicsCard() => _computer.HasGraphicsCard = true;
-9  
-10      public Computer GetResult() => _computer;
-11  }
-12  
-13  public class ComputerDirector
-14  {
-15      public void Build(IComputerBuilder builder)
-16      {
-17          builder.SetCPU();
-18          builder.SetRAM();
-19          builder.SetStorage();
-20          builder.SetGraphicsCard();
-21      }
-22  }
+  public class GamingComputerBuilder : IComputerBuilder
+  {
+      private Computer _computer = new Computer();
+ 
+      public void SetCPU() => _computer.CPU = "Intel Core i7";
+      public void SetRAM() => _computer.RAM = "32 GB";
+      public void SetStorage() => _computer.Storage = "1 TB SSD";
+      public void SetGraphicsCard() => _computer.HasGraphicsCard = true;
+  
+      public Computer GetResult() => _computer;
+  }
+  
+  public class ComputerDirector
+  {
+      public void Build(IComputerBuilder builder)
+      {
+          builder.SetCPU();
+          builder.SetRAM();
+          builder.SetStorage();
+          builder.SetGraphicsCard();
+      }
+  }
 
 //Демонстрація використання Builder:
-1  IComputerBuilder builder = new GamingComputerBuilder();
-2  ComputerDirector director = new ComputerDirector();
-3  
-4  director.Build(builder);
-5  Computer computer = builder.GetResult();
-6  
-7  Console.WriteLine(computer.CPU);
-8  Console.WriteLine(computer.RAM);
-9  Console.WriteLine(computer.Storage);
-10  Console.WriteLine(computer.HasGraphicsCard);
+  IComputerBuilder builder = new GamingComputerBuilder();
+  ComputerDirector director = new ComputerDirector();
+  
+  director.Build(builder);
+  Computer computer = builder.GetResult();
+  
+  Console.WriteLine(computer.CPU);
+  Console.WriteLine(computer.RAM);
+  Console.WriteLine(computer.Storage);
+  Console.WriteLine(computer.HasGraphicsCard);
 
